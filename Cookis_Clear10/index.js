f08682634bec065dd67f37f7643a1d3c0dd6374d
MD. ABU TAHER <abutaher77794@gmail.com>
2024-12-30 20:07:24 +0600

    Clear Cookis

---
+++ Cookis_Clear10/index.js
@@ -0,0 +1,45 @@
+
+let express = require('express');
+
+let app = express();
+app.get('/one', (req, res) => {
+    res.cookie('Name','Taher')
+    res.cookie('age','32')
+    res.end('Cooki set successful')
+})
+app.get('/two', (req, res) => {
+    res.clearCookie('name')
+    res.clearCookie('age')
+    
+    res.end('Cooki Clear successful')
+})
+
+
+
+app.listen(3001,function(){
+    console.log('Server started on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
